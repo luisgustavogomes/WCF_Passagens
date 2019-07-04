9b35d9ec6405c620481d3ab75e07753c375fde58
luis gomes <luisgustavogomes@outlook.com>
2019-07-04 12:35:35 -0300

    Adicionar arquivos de projeto.

---
+++ Passagens/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passagens
+{
+    [DataContract]
+    public class Cliente
+    {
+        [DataMember]
+        public string Nome { get; set; }
+        [DataMember]
+        public string Cpf { get; set; }
+
+    }
+
+    public class ClienteDao
+    {
+        private static List<Cliente> _clientes = new List<Cliente>();
+
+        
+        public void Add(Cliente c)
+        {
+            ClienteDao._clientes.Add(c);
+        }
+
+        public Cliente GetCliente (string nome)
+        {
+            return ClienteDao._clientes.Where(c => c.Nome.Equals(nome)).FirstOrDefault();
+        }
+
+    }
+}
---
+++ Passagens/ClienteService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passagens
+{
+    public class ClienteService : IClienteService
+    {
+
+        private readonly ClienteDao _dao;
+
+        public ClienteService()
+        {
+            _dao = new ClienteDao();
+        }
+
+
+        public void Add(Cliente c)
+        {
+            _dao.Add(c);
+        }
+
+        public Cliente GetCliente(string nome)
+        {
+            return _dao.GetCliente(nome);
+        }
+    }
+}
